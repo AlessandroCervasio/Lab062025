77766a492d2b9be39cc550941e44702b538892f4
AlessandroCervasio <alessandrocervasio12@libero.it>
2026-04-15 10:19:57 +0200

    completato punto 3_ selezione nessun filtro

--- UI/controller.py
+++ UI/controller.py
@@ -18,12 +18,14 @@ class Controller:
 
     def fill_dd_anni(self):
         anni=self._model.getAllAnni()
+        self._view.dd_anno.options.append(ft.dropdown.Option("Nessun filtro"))
         for i in anni:
             self._view.dd_anno.options.append(ft.dropdown.Option(key=str(i["year"]),data=int(i["year"]), text=str(i["year"])))
         self._view.update_page()
 
     def fill_dd_brand(self):
         brand = self._model.getAllBrand()
+        self._view.dd_brand.options.append(ft.dropdown.Option("Nessun filtro"))
         for i in brand:
             self._view.dd_brand.options.append(
                 ft.dropdown.Option(key=str(i["Product_brand"]), data=str(i["Product_brand"]), text=str(i["Product_brand"])))
@@ -32,6 +34,7 @@ class Controller:
 
     def fill_dd_retailer(self):
         retailers = self._model.getAllRetailers()
+        self._view.dd_retailer.options.append(ft.dropdown.Option("Nessun filtro"))
         for i in retailers:
             self._view.dd_retailer.options.append(
                 ft.dropdown.Option(key=i, data=retailers[i], text=str(retailers[i])))
@@ -40,13 +43,16 @@ class Controller:
 
     def handleTop5Vendite(self, e):
         self._view.lv_out.controls.clear()
-
+        string = "Nessun filtro"
         anno=self._view.dd_anno.value
         brand=self._view.dd_brand.value
         retailer=self._view.dd_retailer.value
 
         if anno is None or brand is None or retailer is None:
             self._view.create_alert("Selezionare tutti i parametri per effettuare la ricerca!")
+            return
+        if anno ==string or brand == string or retailer ==string:
+            self._view.create_alert("Parametri non validi per la ricerca.")
             return
 
         topVendite=self._model.handleTop5Vendite( anno, brand, retailer)
@@ -61,11 +67,14 @@ class Controller:
 
     def handleAnalizzaVenditore (self, e):
         self._view.lv_out.controls.clear()
+        string = "Nessun filtro"
 
         anno = self._view.dd_anno.value
         brand = self._view.dd_brand.value
         retailer = self._view.dd_retailer.value
-        if anno is None and brand is None and retailer is None:
+
+
+        if anno == string and brand == string and retailer == string:
             tupla_AllStatistiche=self._model.handleAllStatistiche()
             self._view.lv_out.controls.append(ft.Text("Statistiche globali delle vendite:"))
             self._view.lv_out.controls.append(ft.Text(f"Giro d'affari: {tupla_AllStatistiche[0]}"))
